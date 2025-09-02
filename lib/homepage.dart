157282ffb70cccb7394ad7af9191c398b55860c3
Aniket D. <aniket9370204@gmail.com>
2025-09-02 12:00:02 +0530

    sec comm

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -76,7 +76,7 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text(widget.title),
         ),
         body: Center(
-          // Temporarily commented out PageView until GoogleNavBar is added
+
 
           child: PageView(
             controller: _pageController,
